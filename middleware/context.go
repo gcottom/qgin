c5de1737477a6bc3834e02e9e6a2851707c84c8e
Gage Cottom <gagecottom@hotmail.com>
2024-08-07 18:12:48 -0500

    fix panic

--- middleware/context.go
+++ middleware/context.go
@@ -18,6 +18,7 @@ func ContextMiddleware(baseCtx context.Context) gin.HandlerFunc {
 			logger = zaplog.GetLoggerFromContext(baseCtx).With(zap.String("request_id", ctx.GetHeader(ReqIDHeader))).WithOptions(zap.AddCallerSkip(1))
 		}
 		ctx.Request = ctx.Request.WithContext(context.WithValue(context.WithValue(ctx.Request.Context(), "logger", logger), "request_id", ctx.GetHeader(ReqIDHeader)))
+		ctx.Set("logger", logger)
 		ctx.Next()
 	}
 }

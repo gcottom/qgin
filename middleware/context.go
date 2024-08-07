c7d391de68d1e876a1d66d3fd14a6c0445532d9a
Gage Cottom <gagecottom@hotmail.com>
2024-08-07 18:23:36 -0500

    context fix

--- middleware/context.go
+++ middleware/context.go
@@ -13,12 +13,14 @@ var LogRequestID bool
 
 func ContextMiddleware(baseCtx context.Context) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		var logger *zap.Logger
-		if InjectRequestIDCTX && LogRequestID {
-			logger = zaplog.GetLoggerFromContext(baseCtx).With(zap.String("request_id", ctx.GetHeader(ReqIDHeader))).WithOptions(zap.AddCallerSkip(1))
+		if InjectRequestIDCTX {
+			baseCtx = context.WithValue(baseCtx, "request_id", ctx.GetHeader("request_id"))
 		}
-		ctx.Request = ctx.Request.WithContext(context.WithValue(context.WithValue(ctx.Request.Context(), "logger", logger), "request_id", ctx.GetHeader(ReqIDHeader)))
-		ctx.Set("logger", logger)
+		if InjectRequestIDCTX && LogRequestID {
+			logger := zaplog.GetLoggerFromContext(baseCtx).With(zap.String("request_id", ctx.GetHeader(ReqIDHeader))).WithOptions(zap.AddCallerSkip(1))
+			baseCtx = context.WithValue(baseCtx, "logger", logger)
+		}
+		ctx.Request = ctx.Request.WithContext(baseCtx)
 		ctx.Next()
 	}
 }
